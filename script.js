300c10cdacaead87200f837070394fa55108fc89
Pedro Henrique <pedrohensantosaraujo@gmail.com>
2023-01-27 16:29:09 -0300

    add project to github

---
+++ script.js
@@ -0,0 +1,32 @@
+import {Modal} from './modal.js'
+import {AlertError} from './alert.js'
+import {calculateIMC, notNumber} from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+form.onsubmit = event => {
+    event.preventDefault()
+    const weight = inputWeight.value
+    const height = inputHeight.value
+    
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+    
+    if(weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+    AlertError.close()
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result){
+    const message = `Seu IMC é de ${result}`
+    Modal.message.innerText = message
+    Modal.open()
+}
+
